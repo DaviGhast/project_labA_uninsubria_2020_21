f233c7484ea33246623d734b7cab97359a69d291
DaviGhast <davide.mainardi2001@hotmail.com>
2021-05-21 11:44:17 +0200

    Aggiunta Commenti a MainClass.java e a HomeController.java

--- src/Home/HomeController.java
+++ src/Home/HomeController.java
@@ -1,4 +1,19 @@
 package Home;
 
+/**
+ * La HomeController è la classe per gestire il funzionamento della HomePage grafica.
+ * <p>
+ *     Questa classe ha lo scopo di completare e controllare il funzionamento degli elementi GUI.
+ *     Questa classe è complementare al file HomePage.fxml
+ * </p>
+ * <p>
+ *     La prima finestra della GUI, è la Homepage
+ * </p>
+ * @author Davide Mainardi
+ * @author Marc Cepraga
+ * @author Luca Muggiasca
+ * @author Brenno Re
+ */
+
 public class HomeController {
 }
--- src/MainClass.java
+++ src/MainClass.java
@@ -4,8 +4,30 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * La MainClass è la classe principale del programma.
+ * <p>
+ *     Questa classe ha lo scopo di lanciare la prima pagina della GUI.
+ * </p>
+ * <p>
+ *     La prima finestra della GUI, è la Homepage
+ * </p>
+ * @author Davide Mainardi
+ * @author Marc Cepraga
+ * @author Luca Muggiasca
+ * @author Brenno Re
+ */
+
 public class MainClass extends Application {
 
+    /**
+     * Metodo Start
+     * <p>
+     *     In questo metodo viene caricato il file della GUI HomePage.fxml per essere avviato
+     * </p>
+     * @param primaryStage
+     * @throws Exception
+     */
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
