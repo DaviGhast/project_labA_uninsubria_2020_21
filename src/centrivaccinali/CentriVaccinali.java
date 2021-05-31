03995a8658f7965242d5aa0d6e9ac73e2b6bf0a9
marccepraga <Vivomarc20>
2021-05-31 14:12:31 +0200

    Ho creato i metodi Lettura e CreaFile

--- src/centrivaccinali/CentriVaccinali.java
+++ src/centrivaccinali/CentriVaccinali.java
@@ -1,4 +1,6 @@
+
 package centrivaccinali;
+import java.io.*;
 
 /**
  * CentriVaccinali è la classe principale della sotto applicazione Centri Vaccinali.
@@ -12,7 +14,73 @@ package centrivaccinali;
 public class CentriVaccinali {
 
     public static void main(String[] args) {
+       /* if(!Verifica()){
+            System.out.println("Non esiste");
+        }else{
+            System.out.println("Esiste");
+        }*/
+        CreaFile();
+        Lettura();
+
 
     }
 
+    public static boolean Verifica() {
+       File f = new File("../project_labA_uninsubria_2020_21/data/CentriVaccinali.dati.csv");
+       if(f.exists()){
+           return true;
+       }else{
+           return false;
+       }
+
+    }
+
+
+    public static void CreaFile(){
+        try {
+            File file = new File("../project_labA_uninsubria_2020_21/data/CentriVaccinali.dati.csv");
+            if(!Verifica()){
+                file.createNewFile();
+                System.out.println("Il file è stato creato");
+
+            }else{
+                System.out.println("Il file esiste");
+            }
+
+        }catch(IOException e){
+            System.out.println("Errore");
+            e.printStackTrace();
+        }
+    }
+
+    public static void Lettura() {
+        String file = "../project_labA_uninsubria_2020_21/data/CentriVaccinali.dati.csv";
+        BufferedReader reader = null;
+        String line = "";
+
+        try{
+            reader = new BufferedReader(new FileReader(file));
+            while ((line = reader.readLine()) != null) {
+                String [] row = line.split(",");
+                for(String index : row) {
+                    System.out.printf("%-10s", index);
+                }
+                System.out.println();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
 }
+
+
+
+
+
+
