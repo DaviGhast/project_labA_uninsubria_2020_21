19aa93b46dea8281b0adc04eb54fe800349824b9
marccepraga <cepragamarc1@gmail.com>
2021-06-01 12:23:12 +0200

    Ho creato la classe CittadiniVaccinati

---
+++ src/centrivaccinali/CittadiniVaccinati.java
@@ -0,0 +1,14 @@
+package centrivaccinali;
+
+
+/**
+ * CittadiniVaccinati è la classe principale della sotto applicazione Cittadini Vaccinati.
+ *
+ * @author Davide Mainardi
+ * @author Marc Cepraga
+ * @author Luca Muggiasca
+ * @author Brenno Re
+ */
+
+public class CittadiniVaccinati {
+}
