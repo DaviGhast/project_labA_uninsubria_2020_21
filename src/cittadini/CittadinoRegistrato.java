a68340df7dcabd8360689ecb2c3c0d169f6d2d01
DaviGhast <davide.mainardi2001@hotmail.com>
2021-06-03 15:26:18 +0200

    Creazione classe CittadinoRegistrato

---
+++ src/cittadini/CittadinoRegistrato.java
@@ -0,0 +1,17 @@
+package cittadini;
+
+/**
+ * Classe <code>CittadinoRegistrato</code>
+ * <p>
+ *     Questa classe ha lo scopo di descrivere l'oggetto CittadinoRegistrato
+ * </p>
+ * @author Davide Mainardi
+ * @author Marc Cepraga
+ * @author Luca Muggiasca
+ * @author Brenno Re
+ */
+public class CittadinoRegistrato {
+
+
+
+}
