22056880d08fe5b4f77cbf2bd51ad93f6af3ec2c
DaviGhast <davide.mainardi2001@hotmail.com>
2021-06-03 11:28:52 +0200

    Creazione metodi e javadoc nella classe GestioneCSV

--- src/gestionefile/GestioneCSV.java
+++ src/gestionefile/GestioneCSV.java
@@ -3,7 +3,10 @@ package gestionefile;
 import java.io.File;
 
 /**
- * La classe <code>GestioneCSV</code> ha lo scopo di fornire e raggruppare tutti i metodi che servono per gestire il file CSV
+ * Classe <code>GestioneCSV</code>
+ * <p>
+ *     Questa classe ha lo scopo di fornire e raggruppare tutti i metodi che servono per gestire il file CSV
+ * </p>
  * <p>
  *     I metodi contenuti in questa classe gestiscono operazioni piccole e grandi per la gestione del file CSV,
  *     alcuni per fare operazioni grandi e complesse hanno bisogno di richiamare altre operazioni più piccole.
@@ -15,13 +18,24 @@ import java.io.File;
  */
 public class GestioneCSV {
 
+    /**
+     * Attributo <code>pathFile</code>
+     * <p>
+     *     è una stringa che contine la directory dei file CSV
+     * </p>
+     */
     private String pathFile = "../project_labA_uninsubria_2020_21/data/";
     private File file;
 
+    /**
+     * Metodo costruttore <code>GestioneCSV</code>
+     * @param filename nome del file che concatenato con <code>pathFile</code> da la path completa
+     */
     public GestioneCSV (String filename) {
         pathFile.concat(filename);
         file = new File(pathFile);
     }
+
 
     public void Verifica () {
 
