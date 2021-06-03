fddf8724c6c794ea740f21aeedbc86994f2788b3
DaviGhast <davide.mainardi2001@hotmail.com>
2021-06-03 13:32:45 +0200

    Creazione classe Cittadini

---
+++ src/cittadini/Cittadini.java
@@ -0,0 +1,25 @@
+package cittadini;
+
+/**
+ * Classe <code>Cittadini</code>
+ * <p>
+ *     Questa classe ha lo scopo di contenere i metodi e il main della sotto applicazione per i cittadini
+ * </p>
+ * @author Davide Mainardi
+ * @author Marc Cepraga
+ * @author Luca Muggiasca
+ * @author Brenno Re
+ */
+public class Cittadini {
+
+    /**
+     * Metodo <code>main</code>
+     * <p>
+     *     ha lo scopo di lanciare la sotto applicazione per i cittadini
+     * </p>
+     */
+    public static void main () {
+
+    }
+
+}
