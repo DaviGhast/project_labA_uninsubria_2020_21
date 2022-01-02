58e3b8fe40b6ddb1a4dbb9e95947a67755850ceb
DaviGhast <davide.mainardi2001@hotmail.com>
2022-01-02 18:06:32 +0100

    Aggiornamento Design app

--- src/cittadini/GestioneCittadinoRegistrato.java
+++ src/cittadini/GestioneCittadinoRegistrato.java
@@ -101,7 +101,6 @@ public class GestioneCittadinoRegistrato extends GestioneCsv {
      */
     public boolean rispostaCittadinoEsiste(String userid, String password){
         boolean risposta = false;
-        // TODO: 30/12/2021  aggiungere login con email
         if (ricercaAttrEsiste(userid, "userid")){
             CittadinoRegistrato cittadinoRegistrato = getCittadinoRegistrato(userid);
             if (cittadinoRegistrato.getPassword().equals(AlgoritmoMD5.converti(password))){
